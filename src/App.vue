7e636e99dad8e5e3e3c1c0db42e79ddac17ba9a4
lotinmaria <mariaholotin2007@gmail.com>
2024-03-27 13:22:01 -0300

    Alterando as cores e adicionando parte do resultado

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ const usuario = {
   cidade: '',
   estado: '',
   hobbies: '',
-  programacao: 'asd',
+  programacao: '',
   biografia: ''
 }
 const confirmaSenha = ''
@@ -15,27 +15,44 @@ const senhaConfirmada = usuario.senha == confirmaSenha ? true : false
 </script>
 
 <template>
-  <h1 class="titulo">Formulário</h1>
-  <div class="form">
-    <form class="form-info" action="">
-      <input class="input-user" type="text" placeholder="Nome" required />
-      <input class="input-user" type="text" placeholder="Data de nascimento" required />
-      <input class="input-user" type="email" placeholder="Email" required />
-      <input class="input-user" type="password" placeholder="Senha" required />
-      <input class="input-user" type="password" placeholder="Confirma Senha" required />
-      <input class="input-user" type="text" placeholder="Cidade" required />
-      <input class="input-user" type="text" placeholder="Estado" required />
-      <input class="input-user" type="text" placeholder="Endereço" required />
-      <input class="input-user" type="text" placeholder="Hobbies" />
+  <main>  
+    <div class="form">
+      <form class="form-info" action="">
+        <h1 class="titulo">Formulário</h1>
+        <input class="input-user" type="text" placeholder="Nome" required />
+        <input class="input-user" type="text" placeholder="Data de nascimento" required />
+        <input class="input-user" type="email" placeholder="Email" required />
+        <input class="input-user" type="password" placeholder="Senha" required />
+        <input class="input-user" type="password" placeholder="Confirma Senha" required />
+        <input class="input-user" type="text" placeholder="Cidade" required />
+        <input class="input-user" type="select" placeholder="Estado" required />
+        <input class="input-user" type="text" placeholder="Endereço" required />
+        <input class="input-user" type="text" placeholder="Hobbies" />
       <label for="">Linguagem de programação favorita</label>
       <input class="input-user" type="radio" value="JavaScript" />
       <input class="input-user" type="text" placeholder="Biografia" />
       <input class="input-button" type="submit" name="" id="" />
     </form>
   </div>
+  
+  <div class="resultado">
+    <h1>Resultado</h1>
+    <div class="conteudo">
+      conteúdo
+    </div>
+
+
+  </div>
+
+
+</main>
 </template>
 
 <style scoped>
+main{
+  display: flex;
+  justify-content: space-evenly;
+}
 .titulo {
   display: flex;
   justify-content: center;
@@ -46,7 +63,7 @@ const senhaConfirmada = usuario.senha == confirmaSenha ? true : false
   justify-content: center;
 }
 .form-info {
-  background-color: rgb(197, 187, 202);
+  background-color: rgb(107, 158, 141);
   width: 20vw;
   display: flex;
   flex-direction: column;
@@ -66,10 +83,21 @@ const senhaConfirmada = usuario.senha == confirmaSenha ? true : false
   color: rgba(27, 27, 27, 0.678);
 }
 .input-button {
-  background-color: rgb(138, 29, 233);
+  background-color: rgb(14, 44, 33);
   color: aliceblue;
   padding: 5px 10px;
   border: 3px solid black;
   border-radius: 10px;
+}
+.resultado{
+  background-color: rgb(107, 158, 141);
+  width: 20vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 3px solid black;
+  border-radius: 10px;
+  padding: 1% 2%;
+  gap: 8px;
 }
 </style>
